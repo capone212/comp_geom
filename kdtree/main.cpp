5bce7e3aa9fc5a348d08e770013a2131ba5f0729
Anzor Apshev <anzor.apshev@gmail.com>
2023-10-05 13:01:05 +0300

    fix

--- kdtree/main.cpp
+++ kdtree/main.cpp
@@ -1,5 +1,6 @@
 #include <cstdlib>
 #include <memory>
+#include <optional>
 #include <tuple>
 #include <iostream>
 #include <vector>
@@ -15,7 +16,7 @@ struct TPoint
     int X = 0;
     int Y = 0;
 
-    bool operator=(const TPoint& other)
+    bool operator==(const TPoint& other) const
     {
         return X == other.X && Y == other.Y;
     }
@@ -44,24 +45,36 @@ using PNode = std::shared_ptr<TNode>;
 
 struct TNode
 {
+    std::optional<int> X;
+    std::optional<int> Y;
+    PNode Left;
+    PNode Right;
+
     explicit TNode(std::optional<int> x, std::optional<int> y)
         : X(x)
         , Y(y)
-    {
-
-    }
-
-    std::optional<int> X;
-    std::optional<int> Y;
+    { }
 
     bool IsLeaf() const
     {
         return X.has_value() && Y.has_value();
     }
-
-    PNode Left;
-    PNode Right;
 };
+
+void splitByPredicate(
+    const std::vector<TPoint>& input, 
+    std::vector<TPoint>& lower,
+    std::vector<TPoint>& upper,
+    auto predicate)
+{
+    for (const auto& point : input) {
+        if (predicate(point)) {
+            lower.push_back(point);
+        } else {
+            upper.push_back(point);
+        }
+    }
+}
 
 PNode ConstructKdTreeRecursive(const std::vector<TPoint>& orderedByX, const std::vector<TPoint>& orderedByY, int depth = 0)
 {
@@ -76,18 +89,43 @@ PNode ConstructKdTreeRecursive(const std::vector<TPoint>& orderedByX, const std:
         return std::make_shared<TNode>(orderedByX.front().X, orderedByX.front().Y);
     }
 
-    auto root = std::make_shared<TNode>();
+    auto root = std::make_shared<TNode>(std::nullopt, std::nullopt);
 
     std::vector<TPoint> lowerByX;
     std::vector<TPoint> lowerByY;
 
-    std::vector<TPoint> upeprByX;
+    std::vector<TPoint> upperByX;
     std::vector<TPoint> upperByY;
 
-    return {};
+    std::function<bool (TPoint)> lowerThanMedian;
+
+    if (depth % 2 == 0) {
+        // Split by x
+        auto median = orderedByX[(orderedByX.size()) / 2];
+        root->X = median.X;
+        lowerThanMedian = [median] (TPoint point) {
+            return TOrderByX{}(point, median);
+        };
+    } else {
+        // Split by Y
+        auto median = orderedByY[orderedByY.size() / 2];
+        root->Y = median.Y;
+
+        lowerThanMedian = [median] (TPoint point) {
+            return TOrderByY{}(point, median);
+        };
+    }
+
+    splitByPredicate(orderedByX, lowerByX, upperByX, lowerThanMedian);
+    splitByPredicate(orderedByY, lowerByY, upperByY, lowerThanMedian);
+
+    root->Left = ConstructKdTreeRecursive(lowerByX, lowerByY, depth + 1);
+    root->Right = ConstructKdTreeRecursive(upperByY, upperByY, depth + 1);
+
+    return root;
 }
 
-PNode ConstructKdTree(const std::vector<TPoint>& input)
+PNode ConstructKDTree(const std::vector<TPoint>& input)
 {
     auto orderedByX = input;
     std::sort(orderedByX.begin(), orderedByX.end(), TOrderByX{});
@@ -98,6 +136,55 @@ PNode ConstructKdTree(const std::vector<TPoint>& input)
     return ConstructKdTreeRecursive(orderedByX, orderedByY);
 }
 
+
+void TraverseKDTree(PNode root, std::vector<TPoint>& results, TPoint lower, TPoint upper)
+{
+    if (!root) {
+        return;
+    }
+
+    if (root->IsLeaf()) {
+        auto point = TPoint{*root->X, *root->Y};
+
+        if (point == lower ||
+            point == upper || 
+            (TOrderByX()(lower, point) && TOrderByX()(point, lower)))
+        {
+            results.push_back(point);
+        }
+        return;
+    }
+
+    if (root->X) {
+        auto x = *root->X;
+
+        if (x >= lower.X) {
+            TraverseKDTree(root->Left, results, lower, upper);
+        }
+        if (x <= upper.X) {
+            TraverseKDTree(root->Right, results, lower, upper);
+        }
+    } else {
+        auto y = *root->Y;
+
+        if (y >= lower.Y) {
+            TraverseKDTree(root->Left, results, lower, upper);
+        }
+        if (y <= lower.Y) {
+            TraverseKDTree(root->Right, results, lower, upper);
+        }
+    }
+}
+
+std::vector<TPoint> KDTree(const std::vector<TPoint>& input, const TPoint& lower, const TPoint& upper)
+{
+    auto kdTree = ConstructKDTree(input);
+
+    std::vector<TPoint> results;
+    TraverseKDTree(kdTree, results, lower, upper);
+
+    return results;
+}
 
 ////////////////////////////////////////////////////////////////////////////////////
 
@@ -129,12 +216,12 @@ int main()
             .Input = {{-10, -10}, {0, 0}, {10, 10}, {20, 20}},
             .Lower = {0, 0},
             .Upper = {10, 10},
-            .Output = {{0, 0}, {10, 11}},
+            .Output = {{0, 0}, {10, 10}},
         }
     };
 
     for (const auto& testCase : tests) {
-        auto results = BruteForce(testCase.Input, testCase.Lower, testCase.Upper);
+        auto results = KDTree(testCase.Input, testCase.Lower, testCase.Upper);
         std::set<TPoint, TOrderByX> indexed(results.begin(), results.end());
         bool failed = false;
 
